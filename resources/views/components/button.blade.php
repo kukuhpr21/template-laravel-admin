97585fe4bde4b0a45801b257d1d911129fba4b37
Kukuh182 <kukuhpr21@gmail.com>
2024-10-24 06:58:02 +0700

    remove component button

--- resources/views/components/button.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['type', 'bg' => 'yellow', 'textColor' => 'white', 'full' => true])
-@php
-    $bgColor       = "bg-$bg-500 hover:bg-$bg-700";
-    $textColor     = "text-$textColor";
-    $full          = $full ? 'w-full' : 'w-fit';
-    $generateClass = "$bgColor $textColor $full text-lg font-medium rounded-2xl hover:shadow-xl my-3 p-5"
-@endphp
-<button type="{{ $type }}" class="{{ $generateClass }}" {{ $attributes }}>{{ $slot }}</button>

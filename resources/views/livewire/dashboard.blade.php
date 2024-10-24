4ad5f375b6d76f5abc049d4df25bf3a8a796ca87
Kukuh182 <kukuhpr21@gmail.com>
2024-10-24 07:50:53 +0700

    update dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,2 +1,20 @@
-<div>
+<div class="flex flex-col">
+    <div class="flex flex-row gap-2">
+        <x-card addclass="flex flex-col gap-3 w-56">
+            <x-card-title>Total Penjualan</x-card-title>
+            <x-card-description>100jt</x-card-description>
+        </x-card>
+        <x-card addclass="flex flex-col gap-3 w-56">
+            <x-card-title>Total Penjualan</x-card-title>
+            <x-card-description>100jt</x-card-description>
+        </x-card>
+        <x-card addclass="flex flex-col gap-3 w-56">
+            <x-card-title>Total Penjualan</x-card-title>
+            <x-card-description>100jt</x-card-description>
+        </x-card>
+        <x-card addclass="flex flex-col gap-3 w-56">
+            <x-card-title>Total Penjualan</x-card-title>
+            <x-card-description>100jt</x-card-description>
+        </x-card>
+    </div>
 </div>

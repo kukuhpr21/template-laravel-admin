29f4a49de1912709bbbfd8ab45bd9925bfcb792d
Kukuh182 <kukuhpr21@gmail.com>
2024-10-24 23:19:39 +0700

    update layout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -14,12 +14,9 @@
 
         <li class="hs-accordion" id="users-accordion">
           <button type="button" class="hs-accordion-toggle hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100" aria-expanded="true" aria-controls="users-accordion">
-            <svg class="size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
+            <i class="ri-group-line"></i>
             Users
-
-            <svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-
-            <svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
+            <x-hs-accordion/>
           </button>
 
           <div id="users-accordion" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="users-accordion">
@@ -27,10 +24,7 @@
               <li class="hs-accordion" id="users-accordion-sub-1">
                 <button type="button" class="hs-accordion-toggle hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100" aria-expanded="true" aria-controls="users-accordion-sub-1">
                   Sub Menu 1
-
-                  <svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-
-                  <svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
+                  <x-hs-accordion/>
                 </button>
 
                 <div id="users-accordion-sub-1" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="users-accordion-sub-1">
@@ -56,10 +50,7 @@
               <li class="hs-accordion" id="users-accordion-sub-2">
                 <button type="button" class="hs-accordion-toggle hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100" aria-expanded="true" aria-controls="users-accordion-sub-2">
                   Sub Menu 2
-
-                  <svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-
-                  <svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
+                  <x-hs-accordion/>
                 </button>
 
                 <div id="users-accordion-sub-2" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="users-accordion-sub-2">
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,20 +1,20 @@
 <div class="flex flex-col">
-    <div class="flex lg:flex-row flex-col lg:gap-2 gap-4 lg:px-0 px-2">
-        <x-card addclass="flex lg:w-56 w-full flex-col gap-3">
+    <div class="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:gap-2 gap-4 lg:px-0 px-2">
+        <x-card addclass="flex flex-col gap-3 bg-teal-50">
             <x-card-title>Total Penjualan</x-card-title>
             <x-card-description>100jt</x-card-description>
         </x-card>
-        <x-card addclass="flex lg:w-56 w-full flex-col gap-3">
-            <x-card-title>Total Penjualan</x-card-title>
-            <x-card-description>100jt</x-card-description>
+        <x-card addclass="flex flex-col gap-3 bg-blue-50">
+            <x-card-title>Total Pembeli</x-card-title>
+            <x-card-description>50</x-card-description>
         </x-card>
-        <x-card addclass="flex lg:w-56 w-full flex-col gap-3">
-            <x-card-title>Total Penjualan</x-card-title>
-            <x-card-description>100jt</x-card-description>
+        <x-card addclass="flex flex-col gap-3 bg-yellow-50">
+            <x-card-title>Total Barang</x-card-title>
+            <x-card-description>70</x-card-description>
         </x-card>
-        <x-card addclass="flex lg:w-56 w-full flex-col gap-3">
-            <x-card-title>Total Penjualan</x-card-title>
-            <x-card-description>100jt</x-card-description>
+        <x-card addclass="flex flex-col gap-3 bg-red-50">
+            <x-card-title>Keuntungan</x-card-title>
+            <x-card-description>50jt</x-card-description>
         </x-card>
     </div>
 </div>

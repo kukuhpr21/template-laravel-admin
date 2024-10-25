f01892070657dd4b1c3ccc506531365899b94efa
kukuhpr21 <kukuhpr21@gmail.com>
2024-10-25 12:33:23 +0700

    update dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,19 +1,19 @@
 <x-page title="Dashboard">
     <div class="flex flex-col">
-        <div class="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:gap-2 gap-4 lg:px-0 px-2">
-            <x-card addclass="flex flex-col gap-3 bg-teal-50">
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:gap-4 gap-6 lg:px-0 px-2">
+            <x-card addclass="flex flex-col gap-3 bg-teal-100">
                 <x-card-title>Total Penjualan</x-card-title>
                 <x-card-description>100jt</x-card-description>
             </x-card>
-            <x-card addclass="flex flex-col gap-3 bg-blue-50">
+            <x-card addclass="flex flex-col gap-3 bg-blue-100">
                 <x-card-title>Total Pembeli</x-card-title>
                 <x-card-description>50</x-card-description>
             </x-card>
-            <x-card addclass="flex flex-col gap-3 bg-yellow-50">
+            <x-card addclass="flex flex-col gap-3 bg-yellow-100">
                 <x-card-title>Total Barang</x-card-title>
                 <x-card-description>70</x-card-description>
             </x-card>
-            <x-card addclass="flex flex-col gap-3 bg-red-50">
+            <x-card addclass="flex flex-col gap-3 bg-red-100">
                 <x-card-title>Keuntungan</x-card-title>
                 <x-card-description>50jt</x-card-description>
             </x-card>

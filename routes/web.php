86d9b38c8c2c1b344fc0fb15885926e4d0699025
Kukuh182 <kukuhpr21@gmail.com>
2024-11-01 20:03:37 +0700

    add route choose role and logout

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
-use App\Http\Middleware\EnsureSessionIsValid;
 use Illuminate\Support\Facades\Route;
+use App\Services\Session\SessionService;
+use App\Http\Middleware\EnsureSessionIsValid;
 
 
 Route::middleware([EnsureSessionIsValid::class])->group(function () {
     Route::get('/login', \App\Livewire\Login::class)->name('login');
+
+    Route::get('/choose-role', \App\Livewire\ChooseRole::class)->name('choose-role');
+
     Route::get('/', \App\Livewire\Dashboard::class)->name('dashboard');
+
+    Route::get('/logout', function () {
+        $sessionService = new SessionService();
+        $sessionService->deleteMain();
+        return redirect()->route('login');
+    })->name('logout');
 });

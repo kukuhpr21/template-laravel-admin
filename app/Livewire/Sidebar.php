c895828e8b7b16910a4351b1fac76a5c774ad4c0
Kukuh182 <kukuhpr21@gmail.com>
2024-11-03 03:13:55 +0700

    add sidebar livewire

---
+++ app/Livewire/Sidebar.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Services\Session\SessionService;
+use Livewire\Component;
+
+class Sidebar extends Component
+{
+    private SessionService $sessionService;
+
+    public function render()
+    {
+        $this->sessionService = new SessionService();
+
+        $menus = $this->sessionService->get('menus');
+
+        $menusDecode = json_decode($menus, true);
+
+        $data = [
+            'menus' => $this->makeTreeSidebar($menusDecode)
+        ];
+
+        return view('livewire.sidebar', $data);
+    }
+
+    private function makeTreeSidebar($menus)
+    {
+        $tree = '';
+
+        foreach ($menus as $item) {
+            $isActive = implode('/', request()->segments()) === $item['link'] ? 'bg-gray-200' : '';
+
+            $icon = '';
+
+            if ($item['icon'] != '#') {
+                $icon = '<i class="'.$item['icon'].' ri-lg"></i>';
+            }
+
+            if ($item['children']) {
+                $id = $item['id'].'accordion';
+                $tree .= '<li class="hs-accordion" id="'.$id.'">';
+                $tree .= '<button type="button" class="hs-accordion-toggle '. $isActive .'  hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100" aria-expanded="true" aria-controls="'.$id.'">';
+                $tree .= $icon;
+                $tree .= $item['name'];
+                $tree .= '<svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>';
+                $tree .= '<svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>';
+                $tree .= '</button>';
+                $tree .= '<div id="'.$id.'" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="'.$id.'">';
+                $tree .= '<ul class="hs-accordion-group ps-3 pt-2" data-hs-accordion-always-open>';
+                $tree .= self::makeTreeSidebar($item['children']);
+                $tree .= '</ul>';
+            } else {
+                $tree .= '<li>';
+                $tree .= '<a class="flex items-center gap-x-3.5 py-2 px-2.5 '. $isActive .' text-sm text-gray-700 rounded-lg hover:bg-gray-100" href="'.$item['link'].'">';
+                $tree .= $icon;
+                $tree .= $item['name'];
+                $tree .= '</a>';
+            }
+
+           $tree .= '</li>';
+        }
+        return $tree;
+    }
+}
---
+++ resources/views/livewire/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div id="sidebar" class="hs-overlay [--auto-close:lg] hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed  top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 overflow-auto" role="dialog" tabindex="-1" aria-label="Sidebar">
+    <div class="px-6">
+        <x-logo />
+    </div>
+    <nav class="hs-accordion-group p-6 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
+        <ul class="space-y-1.5">
+            {{-- { !! $menus !! } --}}
+            {!! $menus !!}
+        </ul>
+    </nav>
+</div>

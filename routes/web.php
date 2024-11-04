cab5150f011bb4e016ed1314f165dd9b051a15f9
kukuhpr21 <kukuhpr21@gmail.com>
2024-11-04 09:19:37 +0700

    handle empty path

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@ Route::middleware([EnsureSessionIsValid::class])->group(function () {
 
     Route::get('/choose-role', \App\Livewire\Guest\ChooseRole::class)->name('choose-role');
 
+    Route::get('/', function() {
+        return redirect()->route('dashboard');
+    });
+
     Route::get('/dashboard', \App\Livewire\Main\Dashboard::class)->name('dashboard');
+
     Route::get('/settings/roles', \App\Livewire\Main\Roles\Index::class)->name('roles');
 
     Route::get('/logout', function () {

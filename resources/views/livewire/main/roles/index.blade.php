403d1a0277ee3d880e1afe1d396dc71bffd4b5d1
Kukuh182 <kukuhpr21@gmail.com>
2024-11-06 20:06:38 +0700

    update template list role

--- resources/views/livewire/main/roles/index.blade.php
+++ resources/views/livewire/main/roles/index.blade.php
@@ -1,10 +1,3 @@
 <x-page title="Daftar Role">
-    {{-- <div>
-        @foreach ($data as $item)
-            <span>{{ $item->name }}</span>
-        @endforeach
-    </div> --}}
-
     <livewire:main.roles.role-table></livewire:main.roles.role-table>
-
 </x-page>

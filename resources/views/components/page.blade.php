c7dcd12e15df535ac1bff55732d0cd17b8203365
Kukuh182 <kukuhpr21@gmail.com>
2024-11-06 20:19:54 +0700

    init button back

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -1,6 +1,13 @@
-@props(['title'])
+@props(['title', 'showButtonBack' => true])
 <div class="flex flex-col w-full h-screen overflow-auto p-3 bg-slate-100 rounded-xl drop-shadow-md shadow-black">
-    <span class="text-lg text-gray-500 font-semibold">{{ $title }}</span>
+    <div class="flex flex-row gap-2 items-center">
+        @if ($showButtonBack)
+            <a href="{{ url()->previous() }}" class="hover:bg-slate-100 hover:drop-shadow-md hover:rounded-lg p-2">
+                <i class="ri-arrow-left-fill text-slate-600"></i>
+            </a>
+        @endif
+        <span class="text-lg text-gray-500 font-semibold">{{ $title }}</span>
+    </div>
     <div class="py-4">
         {{ $slot }}
     </div>

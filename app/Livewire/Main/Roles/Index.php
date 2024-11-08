08981b8afc74f6ef50f73b83ca79b60e4bf47f9a
Kukuh182 <kukuhpr21@gmail.com>
2024-11-08 23:01:17 +0700

    remove unused

--- app/Livewire/Main/Roles/Index.php
+++ app/Livewire/Main/Roles/Index.php
@@ -5,18 +5,11 @@ namespace App\Livewire\Main\Roles;
 use App\Models\Menu;
 use App\Models\Role;
 use Livewire\Component;
-use Livewire\WithPagination;
-use Livewire\WithoutUrlPagination;
 
 class Index extends Component
 {
-    use WithPagination;
     public function render()
     {
-        // dd(Menu::paginate(5)->toArray());
-        return view('livewire.main.roles.index', [
-            'columns' => ['id', 'name'],
-            'data' => Menu::paginate(5)
-        ]);
+        return view('livewire.main.roles.index');
     }
 }

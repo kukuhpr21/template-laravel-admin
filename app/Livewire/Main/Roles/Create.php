32b98628ec3a8addc2e484890b09b6cb83832af6
Kukuh182 <kukuhpr21@gmail.com>
2024-11-08 23:03:08 +0700

    update module save role

--- app/Livewire/Main/Roles/Create.php
+++ app/Livewire/Main/Roles/Create.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire\Main\Roles;
 
+use App\Services\Role\RoleService;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Create extends Component
 {
+    private RoleService $roleService;
+
+    #[Validate(['required', 'unique:roles,name'])]
+    public string $name = "";
+
+    public function submit()
+    {
+        $request = $this->validate();
+
+        $this->roleService = new RoleService();
+        $result = $this->roleService->save($request['name']);
+
+        $this->reset();
+
+        $icon    = $result ? 'success' : 'error';
+        $message = ($result ? 'Berhasil' : 'Gagal')." menambahkan data";
+        $this->dispatch('sweet-alert-notif', icon: $icon, title: $message);
+
+        session()->flash('notif', [
+            'status' => $icon,
+            'message' => $message
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.main.roles.create');

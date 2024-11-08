a9df64d4d5832a4d7c95b6d0ce12ffbf5788cb76
Kukuh182 <kukuhpr21@gmail.com>
2024-11-08 19:26:24 +0700

    add drop shadow

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -4,4 +4,4 @@
     $model = $form ? 'form.'.$lowerName : $lowerName;
 @endphp
 <input type="{{ $type }}" wire:model="{{$model}}" placeholder="{{ $placeholder }}" name="{{ $lowerName }}" id="{{ $lowerName }}"
-    class="{{ $class }} py-4 px-3 my-3 rounded-2xl focus:outline-none focus:ring-0 border-0 ">
+    class="{{ $class }} py-4 px-3 my-3 rounded-2xl focus:outline-none focus:ring-0 border-0 hover:drop-shadow-xl">

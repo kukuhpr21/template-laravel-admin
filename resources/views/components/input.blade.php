7f0088f9581b3a60bea5507c9447e45824ba0646
kukuhpr21 <kukuhpr21@gmail.com>
2024-11-11 16:16:11 +0700

    add shadow on focus

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -4,4 +4,4 @@
     $model = $form ? 'form.'.$lowerName : $lowerName;
 @endphp
 <input type="{{ $type }}" wire:model="{{$model}}" placeholder="{{ $placeholder }}" name="{{ $lowerName }}" id="{{ $lowerName }}"
-    class="{{ $class }} py-4 px-3 my-3 rounded-2xl focus:outline-none focus:ring-0 border-0 hover:drop-shadow-xl">
+    class="{{ $class }} py-4 px-3 my-3 rounded-2xl focus:outline-none focus:ring-0 border-0 hover:drop-shadow-xl focus:drop-shadow-xl">

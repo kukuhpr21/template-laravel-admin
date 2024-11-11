3bace9c85e328daf10f85e5464685707199dcaab
kukuhpr21 <kukuhpr21@gmail.com>
2024-11-11 16:47:50 +0700

    add min height

--- resources/views/components/card-map.blade.php
+++ resources/views/components/card-map.blade.php
@@ -1,5 +1,5 @@
 @props(['color' => 'bg-white', 'title' => 'Map', 'description' => 'no description'])
-<div class="{{ $color }} flex flex-col py-7 px-5 gap-2 rounded-lg hover:drop-shadow-xl">
+<div class="{{ $color }} flex flex-col py-7 px-5 gap-2 rounded-lg hover:drop-shadow-xl h-scree sm:min-h-80">
     <x-card-title>{{ $title }}</x-card-title>
     <div class="flex h-full rounded-lg" {{ $attributes }}>
         {{ $slot }}

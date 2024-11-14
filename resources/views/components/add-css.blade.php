8b8e519364f5e90ebf8b64c96e149f7518cc045d
Kukuh182 <kukuhpr21@gmail.com>
2024-11-14 23:59:43 +0700

    remove leaflet option

--- resources/views/components/add-css.blade.php
+++ resources/views/components/add-css.blade.php
@@ -2,11 +2,5 @@
 
 @section('css')
     @switch($add)
-        @case('leaflet')
-            <!-- Leaflet -->
-            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-            @break
-        @default
-
     @endswitch
 @endsection
--- resources/views/components/add-js.blade.php
+++ resources/views/components/add-js.blade.php
@@ -2,19 +2,4 @@
 
 
 @switch($add)
-    @case('leaflet')
-        @if ($up)
-            @section('js-up')
-            <!-- Leaflet -->
-            <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-            @endsection
-         @else
-            @push('scripts')
-            <!-- Leaflet -->
-            <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-            @endpush
-        @endif
-        @break
-    @default
-
 @endswitch

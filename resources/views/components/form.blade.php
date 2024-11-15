ded24c1fae3bfff6930d393e72231aaea81e4bdb
Kukuh182 <kukuhpr21@gmail.com>
2024-11-15 23:17:43 +0700

    change color form

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,4 +1,4 @@
 @props(['action' => 'submit', 'width' => 'w-full'])
-<form wire:submit="{{ $action }}" class="flex flex-col gap-3 py-3 px-4 bg-zinc-100 rounded-lg border-2 border-dotted border-zinc-200 hover:drop-shadow-xl {{ $width }}">
+<form wire:submit="{{ $action }}" class="flex flex-col gap-3 py-3 px-4 bg-blue-50 rounded-xl {{ $width }}">
 {{ $slot }}
 </form>

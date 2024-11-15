7d5743d55df6fc6817e0fc7a340e43d1a29aee65
Kukuh182 <kukuhpr21@gmail.com>
2024-11-15 23:17:22 +0700

    add border

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,8 +2,8 @@
 @php
     $lowerName = strtolower($name);
     $model = $form ? 'form.'.$lowerName : $lowerName;
-    $color = "bg-".$class."-200 focus:bg-".$class."-200";
-    $classes = $color.' py-4 px-3 my-3 rounded-2xl focus:outline-none focus:ring-0 border-0 hover:drop-shadow-xl focus:drop-shadow-xl';
+    $color = "bg-".$class."-50 focus:bg-".$class."-50";
+    $classes = $color.' py-4 px-3 my-3 rounded-2xl border border border-slate-300 focus:outline-none focus:ring-0 hover:drop-shadow-xl focus:drop-shadow-xl';
 @endphp
 
 @if ($type != 'password')
